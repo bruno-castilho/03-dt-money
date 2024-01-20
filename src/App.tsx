10f98fdc082de1bd28b8d87c9b789457d971a0b2
Bruno <bruno.silva.castilho@gmail.com>
2024-01-20 12:43:21 -0300

    config eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { ThemeProvider } from "styled-components";
-import { defaultTheme } from "./styles/themes/default";
-import { GlobalStyle } from "./global";
-import { Transactions } from "./pages/Transactions";
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './global'
+import { Transactions } from './pages/Transactions'
 
 export function App() {
   return (
-  <ThemeProvider theme={defaultTheme}>
-    <GlobalStyle />
-      <Transactions/>
-  </ThemeProvider>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <Transactions />
+    </ThemeProvider>
   )
 }
